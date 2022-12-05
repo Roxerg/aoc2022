cec7590e1743fc80e260c7cb78014dfe55da35d8
roxerg <roxguda@gmail.com>
2022-12-05 21:32:58 +0000

    Prepping for day6 ;)

---
+++ src/day6/mod.rs
@@ -0,0 +1,20 @@
+// use std::env;
+use std::fs;
+use regex::Regex;
+
+
+fn read_input() -> String {
+    let contents = fs::read_to_string("day5/input.txt")
+        .expect("Failed to read input");
+    return contents;
+}
+
+
+
+pub fn part1() {
+    println!("TODO");
+}
+
+pub fn part2() {
+    println!("TODO");
+}
